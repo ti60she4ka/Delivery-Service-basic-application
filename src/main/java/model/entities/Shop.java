06938f97e5f6f85f6294e5b417c71d2758c6758a
ti60she4ka <ti60she4ka@mail.ru>
2021-07-10 13:42:33 +0300

    feature / change shop domain model
    Add new field to shop entity

--- src/main/java/model/entities/Shop.java
+++ src/main/java/model/entities/Shop.java
@@ -1,5 +1,6 @@
 package model.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,36 +9,23 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Shop extends BaseEntity {
     private String name;
     private String city;
-    private List<ProductStorage> productStorages;
-
-    public Shop(String name, String city) {
-        this.name = name;
-        this.city = city;
-        productStorages = new ArrayList<>();
-    }
+    private String address;
 
     public Shop(Shop shop){
         this.name = shop.getName();
         this.city = shop.getCity();
-        this.productStorages = cloneProductStorages(shop.getProductStorages());
-    }
-
-    private List<ProductStorage> cloneProductStorages(List<ProductStorage> productStorages){
-        List<ProductStorage> clone = new ArrayList<>();
-        for(ProductStorage item : productStorages){
-            clone.add(new ProductStorage(item));
-        }
-
-        return clone;
+        this.address = shop.getAddress();
     }
 
     @Override
     public String toString() {
         return "ID — " + id +
                 "\nName — " + name +
-                "\nCity — " + city;
+                "\nCity — " + city +
+                "\nAddress — " + address;
     }
 }
