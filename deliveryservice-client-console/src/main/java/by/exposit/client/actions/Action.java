9fcdaac07fce8c734ca553cbe0b83b5b1d624f2b
ti60she4ka <ti60she4ka@mail.ru>
2021-07-26 17:00:34 +0300

    delete client-console

--- deliveryservice-client-console/src/main/java/by/exposit/client/actions/Action.java
+++
@@ -1,6 +0,0 @@
-package by.exposit.client.actions;
-
-public interface Action {
-
-  void doAction(int index) throws Exception;
-}
--- deliveryservice-client-console/src/main/java/by/exposit/client/initializers/Initializer.java
+++
@@ -1,6 +0,0 @@
-package by.exposit.client.initializers;
-
-public interface Initializer {
-
-  void init();
-}
