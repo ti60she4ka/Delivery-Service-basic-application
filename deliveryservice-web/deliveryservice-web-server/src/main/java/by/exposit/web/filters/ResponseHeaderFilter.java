2625c52615e2be14c1e5fb8dceb179e3df196d1d
ti60she4ka <ti60she4ka@mail.ru>
2021-07-30 11:02:40 +0300

    feature / add ResponseHeaderFilter

---
+++ deliveryservice-web/deliveryservice-web-server/src/main/java/by/exposit/web/filters/ResponseHeaderFilter.java
@@ -0,0 +1,22 @@
+package by.exposit.web.filters;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class ResponseHeaderFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    httpServletResponse.addHeader("Application-Name", "delivery-service");
+    filterChain.doFilter(httpServletRequest, httpServletResponse);
+  }
+}
